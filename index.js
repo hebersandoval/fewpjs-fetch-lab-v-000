47f215262daa2629a41b1b0943fe217fe6e79d64
Heber Sandoval <he3ers@gmail.com>
2020-02-13 03:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
-    .then(function(response) {
+    .then((response) {
       return response.json();
     })
     .then(function(json){
