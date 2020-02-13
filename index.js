0a261e89b5ed5736a6e0c95d660803fd94792f2a
Heber Sandoval <he3ers@gmail.com>
2020-02-13 03:34:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-
-
-function renderBooks(json) {
-  const main = document.querySelector('main')
-  json.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = `<h2>${book.name}</h2>`
-    main.appendChild(h2)
-  })
-}
-
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
     .then((response) => {
@@ -17,6 +6,15 @@ function fetchBooks() {
     .then((json) => {
       renderBooks(json);
     })
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
 }
 
 document.addEventListener('DOMContentLoaded', function() {
